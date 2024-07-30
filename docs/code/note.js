e7a21041714f4c091ae957a54ba8a3a0cd2a7526
Douglas Hanley <thesecretaryofwar@gmail.com>
2024-07-30 11:46:39 -0500

    fix note example

--- docs/code/note.js
+++ docs/code/note.js
@@ -1,6 +1,6 @@
 // add a note to a paricular place
 let line = SymPath({fy: x => 1-x*x, xlim: [-1, 1]});
-let note = Note('1 - x^2', {pos: [0, 1.1], rad: 0.2, latex: true});
+let note = Note('1 - x^2', {pos: [0.025, 1.1], rad: 0.15, latex: true});
 let plot = Plot([line, note], {
   aspect: phi, ylim: [0, 1.5], yticks: 4, grid: true, grid_opacity: 0.1
 });
