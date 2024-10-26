eca4702a14f8d029cd2eb879b22f6aebb669c0b1
Douglas Hanley <thesecretaryofwar@gmail.com>
2024-10-26 17:56:24 -0500

    fix docs typo

--- docs/code/gum.js
+++ docs/code/gum.js
@@ -1,4 +1,4 @@
-// The text "GUM" is a simple frame
+// The text "GUM" in a simple frame
 let text = Text('GUM');
 let frame = Frame(text, {padding: 0.1, border: 1, margin: 0.1});
 return frame;
--- docs/gen/examples.md
+++
@@ -1,3 +0,0 @@
-# Examples
-
-This is an extensive library of `gum.js` examples with user prompts (descriptions of what to create) and the proposed `gum.js` code to create the figure.
